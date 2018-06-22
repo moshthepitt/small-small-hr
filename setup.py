95246b4551f3527b5f536ad1e3893d637502343d
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-22 10:38:35 +0300

    Bump to version 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='small-small-hr',
-    version='0.0.5',
+    version='0.0.6',
     description='Minimal human resource management app for Django',
     license='MIT',
     author='Kelvin Jayanoris',
