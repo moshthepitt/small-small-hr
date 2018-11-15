f635d43ffca34a49bce7d6312b38a09fe00f6e23
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-11-16 00:34:53 +0300

    Add settings module

---
+++ small_small_hr/settings.py
@@ -0,0 +1,13 @@
+"""
+Configurable options
+"""
+SSHR_MAX_CARRY_OVER = 10
+SSHR_DAY_LEAVE_VALUES = {
+    1: 1,  # Monday
+    2: 1,  # Tuesday
+    3: 1,  # Wednesday
+    4: 1,  # Thursday
+    5: 1,  # Friday
+    6: 0,  # Saturday
+    7: 0,  # Sunday
+}
