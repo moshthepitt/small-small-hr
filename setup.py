fb5368040bb6c3a34439dcedf95b92d1671d74d4
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-07 13:36:17 +0300

    Add support for django3, remove support for django 2.0 and 2.1

--- setup.py
+++ setup.py
@@ -39,9 +39,9 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Framework :: Django",
-        "Framework :: Django :: 2.0",
-        "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
     ],
 )
