e4929d5c2204a3895b16325529565096e92cfaf3
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-13 12:55:03 +0300

    Add test for overtime review process

--- tests/test_process.py
+++ tests/test_process.py
@@ -11,8 +11,8 @@ from model_mommy import mommy
 from model_reviews.forms import PerformReview
 from model_reviews.models import ModelReview, Reviewer
 
-from small_small_hr.forms import ApplyLeaveForm
-from small_small_hr.models import Leave, StaffProfile
+from small_small_hr.forms import ApplyLeaveForm, ApplyOverTimeForm
+from small_small_hr.models import Leave, OverTime, StaffProfile
 
 
 class TestProcess(TestCase):  # pylint: disable=too-many-public-methods
@@ -29,8 +29,8 @@ class TestProcess(TestCase):  # pylint: disable=too-many-public-methods
         self.boss.groups.add(hr_group)
 
     @patch("model_reviews.emails.send_email")
-    def test_review_process(self, mock):  # pylint: disable=too-many-locals
-        """Test the review process."""
+    def test_leave_review_process(self, mock):  # pylint: disable=too-many-locals
+        """Test the Leave review process."""
         manager = mommy.make(
             "auth.User", first_name="Jane", last_name="Ndoe", email="jane@example.com"
         )
@@ -150,6 +150,135 @@ class TestProcess(TestCase):  # pylint: disable=too-many-public-methods
                 obj=review,
                 cc_list=None,
                 template="leave",
+                template_path="small_small_hr/email",
+            )
+        )
+        mock.assert_has_calls(expected_calls)
+
+        self.assertEqual(4, mock.call_count)
+
+    @patch("model_reviews.emails.send_email")
+    def test_overtime_review_process(self, mock):  # pylint: disable=too-many-locals
+        """Test the OverTime review process."""
+        manager = mommy.make(
+            "auth.User", first_name="Jane", last_name="Ndoe", email="jane@example.com"
+        )
+        manager_profile = mommy.make("small_small_hr.StaffProfile", user=manager)
+
+        user = mommy.make(
+            "auth.User", first_name="Bob", last_name="Ndoe", email="bob@example.com"
+        )
+        staffprofile = mommy.make("small_small_hr.StaffProfile", user=user)
+        staffprofile.supervisor = manager_profile
+        staffprofile.save()
+
+        # apply for overtime
+        start = datetime(2017, 6, 5, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
+        end = datetime(2017, 6, 5, 6, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
+
+        data = {
+            "staff": staffprofile.id,
+            "date": start.date(),
+            "start": start.time(),
+            "end": end.time(),
+            "review_reason": "Extra work",
+        }
+
+        form = ApplyOverTimeForm(data=data)
+        self.assertTrue(form.is_valid())
+        overtime = form.save()
+
+        # check that a ModelReview object is created
+        obj_type = ContentType.objects.get_for_model(overtime)
+        self.assertEqual(
+            1,
+            ModelReview.objects.filter(
+                content_type=obj_type, object_id=overtime.id
+            ).count(),
+        )
+        review = ModelReview.objects.get(content_type=obj_type, object_id=overtime.id)
+        self.assertTrue(Leave.PENDING, review.review_status)
+
+        # check that email is sent once to the reviewer
+        reviewer = Reviewer.objects.get(review=review, user=manager)
+        boss_reviewer = Reviewer.objects.get(review=review, user=self.boss)
+
+        expected_calls = [
+            call(
+                name="Jane Ndoe",
+                email="jane@example.com",
+                subject="New Request For Approval",
+                message="There has been a new request that needs your attention.",
+                obj=review,
+                cc_list=None,
+                template="overtime",
+                template_path="small_small_hr/email",
+            ),
+            call(
+                name="Boss Lady",
+                email="boss@example.com",
+                subject="New Request For Approval",
+                message="There has been a new request that needs your attention.",
+                obj=review,
+                cc_list=None,
+                template="overtime",
+                template_path="small_small_hr/email",
+            ),
+        ]
+
+        mock.assert_has_calls(expected_calls)
+
+        # approve the review
+        data = {
+            "review": review.pk,
+            "reviewer": reviewer.pk,
+            "review_status": ModelReview.APPROVED,
+        }
+        form = PerformReview(data=data)
+        self.assertTrue(form.is_valid())
+        form.save()
+        review.refresh_from_db()
+        overtime.refresh_from_db()
+        self.assertEqual(ModelReview.APPROVED, review.review_status)
+        self.assertEqual(OverTime.APPROVED, overtime.review_status)
+
+        expected_calls.append(
+            call(
+                name="Bob Ndoe",
+                email="bob@example.com",
+                subject="Your request has been processed",
+                message="Your request has been processed, please log in to view the status.",  # noqa  # pylint: disable=line-too-long
+                obj=review,
+                cc_list=None,
+                template="overtime",
+                template_path="small_small_hr/email",
+            )
+        )
+        mock.assert_has_calls(expected_calls)
+
+        # boss rejects it
+        data = {
+            "review": review.pk,
+            "reviewer": boss_reviewer.pk,
+            "review_status": ModelReview.REJECTED,
+        }
+        form = PerformReview(data=data)
+        self.assertTrue(form.is_valid())
+        form.save()
+        review.refresh_from_db()
+        overtime.refresh_from_db()
+        self.assertEqual(ModelReview.REJECTED, review.review_status)
+        self.assertEqual(OverTime.REJECTED, overtime.review_status)
+
+        expected_calls.append(
+            call(
+                name="Bob Ndoe",
+                email="bob@example.com",
+                subject="Your request has been processed",
+                message="Your request has been processed, please log in to view the status.",  # noqa  # pylint: disable=line-too-long
+                obj=review,
+                cc_list=None,
+                template="overtime",
                 template_path="small_small_hr/email",
             )
         )
