7e142631eff9987af1e059882ce4184517cc7eb6
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-15 22:02:05 +0300

    Add tests for leave application emails

--- tests/test_emails.py
+++ tests/test_emails.py
@@ -1,136 +1,71 @@
-# """Module to test small_small_hr Emails."""
-# from datetime import datetime
-# from unittest.mock import patch
+"""Module to test small_small_hr Emails."""
+from datetime import datetime
 
-# from django.conf import settings
-# from django.test import TestCase, override_settings
+from django.conf import settings
+from django.core import mail
+from django.test import override_settings
 
-# import pytz
-# from model_mommy import mommy
+import pytz
+from freezegun import freeze_time
+from model_mommy import mommy
+from snapshottest.django import TestCase
 
-# from small_small_hr.emails import (
-#     leave_application_email,
-#     leave_processed_email,
-#     overtime_application_email,
-#     overtime_processed_email,
-# )
-# from small_small_hr.models import Leave, OverTime
+from small_small_hr.forms import ApplyLeaveForm
+from small_small_hr.models import Leave, StaffProfile
+from small_small_hr.utils import create_annual_leave
 
 
-# @override_settings(
-#     SSHR_ADMIN_EMAILS=["admin@example.com"],
-#     SSHR_ADMIN_LEAVE_EMAILS=["hr@example.com"],
-#     SSHR_ADMIN_OVERTIME_EMAILS=["ot@example.com"],
-#     SSHR_ADMIN_NAME="mosh",
-# )
-# class TestEmails(TestCase):
-#     """Test class for emails."""
+@override_settings(ROOT_URLCONF="tests.urls")
+class TestEmails(TestCase):
+    """Test class for emails."""
 
-#     def setUp(self):
-#         """Set up."""
-#         self.user = mommy.make(
-#             "auth.User", first_name="Bob", last_name="Ndoe", email="bob@example.com"
-#         )
-#         self.staffprofile = mommy.make("small_small_hr.StaffProfile", user=self.user)
+    maxDiff = None
 
-#     @patch("small_small_hr.emails.send_email")
-#     def test_leave_application_email(self, mock):
-#         """Test leave_application_email."""
-#         start = datetime(2017, 6, 5, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
-#         end = datetime(2017, 6, 10, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
-#         leave = mommy.make(
-#             "small_small_hr.Leave",
-#             staff=self.staffprofile,
-#             start=start,
-#             end=end,
-#             leave_type=Leave.SICK,
-#             review_status=Leave.PENDING,
-#         )
+    def setUp(self):
+        """Set up."""
+        self.user = mommy.make(
+            "auth.User", first_name="Mosh", last_name="Pitt", email="bob@example.com"
+        )
+        self.staffprofile = mommy.make("small_small_hr.StaffProfile", user=self.user)
+        self.staffprofile.leave_days = 17
+        self.staffprofile.sick_days = 9
+        self.staffprofile.save()
+        self.staffprofile.refresh_from_db()
 
-#         leave_application_email(leave)
+        create_annual_leave(self.staffprofile, 2017, Leave.REGULAR)
 
-#         mock.assert_called_with(
-#             name="mosh",
-#             email="hr@example.com",
-#             subject="New Leave Application",
-#             message="There has been a new leave application.  Please log in to process it.",  # noqa  # pylint: disable=line-too-long
-#             obj=leave,
-#             template="leave_application",
-#             template_path="small_small_hr/email",
-#         )
+        StaffProfile.objects.rebuild()
 
-#     @patch("small_small_hr.emails.send_email")
-#     def test_leave_processed_email(self, mock):
-#         """Test leave_processed_email."""
-#         start = datetime(2017, 6, 5, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
-#         end = datetime(2017, 6, 10, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
-#         leave = mommy.make(
-#             "small_small_hr.Leave",
-#             staff=self.staffprofile,
-#             start=start,
-#             end=end,
-#             leave_type=Leave.SICK,
-#             review_status=Leave.APPROVED,
-#         )
+        hr_group = mommy.make("auth.Group", name=settings.SSHR_ADMIN_USER_GROUP_NAME)
+        self.boss = mommy.make(
+            "auth.User", first_name="Mother", last_name="Hen", email="hr@example.com"
+        )
+        self.boss.groups.add(hr_group)
 
-#         leave_processed_email(leave)
-
-#         mock.assert_called_with(
-#             name="Bob Ndoe",
-#             email="bob@example.com",
-#             subject="Your leave application has been processed",
-#             message="You leave application status is Approved.  Log in for more info.",  # noqa  # pylint: disable=line-too-long
-#             obj=leave,
-#             cc_list=["hr@example.com"],
-#             template_path="small_small_hr/email",
-#         )
-
-#     @patch("small_small_hr.emails.send_email")
-#     def test_overtime_application_email(self, mock):
-#         """Test overtime_application_email."""
-#         start = datetime(2017, 6, 5, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
-#         end = datetime(2017, 6, 10, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
-#         overtime = mommy.make(
-#             "small_small_hr.OverTime",
-#             staff=self.staffprofile,
-#             start=start,
-#             end=end,
-#             review_status=OverTime.PENDING,
-#         )
-
-#         overtime_application_email(overtime)
-
-#         mock.assert_called_with(
-#             name="mosh",
-#             email="ot@example.com",
-#             subject="New Overtime Application",
-#             message="There has been a new overtime application.  Please log in to process it.",  # noqa  # pylint: disable=line-too-long
-#             obj=overtime,
-#             template="overtime_application",
-#             template_path="small_small_hr/email",
-#         )
-
-#     @patch("small_small_hr.emails.send_email")
-#     def test_overtime_processed_email(self, mock):
-#         """Test overtime_processed_email."""
-#         start = datetime(2017, 6, 5, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
-#         end = datetime(2017, 6, 10, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
-#         overtime = mommy.make(
-#             "small_small_hr.OverTime",
-#             staff=self.staffprofile,
-#             start=start,
-#             end=end,
-#             review_status=OverTime.REJECTED,
-#         )
-
-#         overtime_processed_email(overtime)
-
-#         mock.assert_called_with(
-#             name="Bob Ndoe",
-#             email="bob@example.com",
-#             subject="Your overtime application has been processed",
-#             message="You overtime application status is Rejected.  Log in for more info.",  # noqa  # pylint: disable=line-too-long
-#             obj=overtime,
-#             cc_list=["ot@example.com"],
-#             template_path="small_small_hr/email",
-#         )
+    @freeze_time("June 1st, 2017")
+    def test_leave_emails(self):
+        """Test Leave emails."""
+        # apply for leave
+        start = datetime(2017, 6, 5, 7, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
+        end = datetime(2017, 6, 10, 7, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
+        data = {
+            "staff": self.staffprofile.id,
+            "leave_type": Leave.REGULAR,
+            "start": start,
+            "end": end,
+            "review_reason": "Need a break",
+        }
+        form = ApplyLeaveForm(data=data)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(
+            "Mosh Pitt requested time off on 05 Jun to 10 Jun", mail.outbox[0].subject
+        )
+        self.assertEqual(["Mother Hen <hr@example.com>"], mail.outbox[0].to)
+        self.assertMatchSnapshot(mail.outbox[0].body)
+        self.assertMatchSnapshot(mail.outbox[0].alternatives[0][0])
+        # import ipdb; ipdb.set_trace()
+        # leave = form.save()
+        # obj_type = ContentType.objects.get_for_model(leave)
+        # review = ModelReview.objects.get(content_type=obj_type, object_id=leave.id)
+        # reviewer = Reviewer.objects.get(review=review, user=self.boss)
