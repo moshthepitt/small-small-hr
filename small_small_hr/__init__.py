b011193fe8d9c8552324af8bac727cf410705903
Kelvin Jayanoris <kjayanoris@gmail.com>
2022-01-31 17:28:06 +0300

    Update

--- small_small_hr/__init__.py
+++ small_small_hr/__init__.py
@@ -1,5 +1,5 @@
 """Main init file for small_small_hr."""
-VERSION = (0, 4, 1)
+VERSION = (0, 4, 2)
 __version__ = ".".join(str(v) for v in VERSION)
 # pylint: disable=invalid-name
 default_app_config = "small_small_hr.apps.SmallSmallHrConfig"  # noqa
